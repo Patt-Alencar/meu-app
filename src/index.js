6d1c9472646aad40256adcc034ef406e4dc5ad9b
Patricia <patt_alencar@yahoo.com.br>
2024-08-17 18:52:37 -0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <Data />
+      <SkillsList />
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Avatar() {
+  return <img className="avatar" src="./foto.red.jpg" alt="foto Patricia" />;
+}
+
+function Data() {
+  return (
+    <div className="data">
+      <h1>Patrícia de Alencar</h1>
+      <p>
+        Front-end developer. I really love to code and I'm looking forward to
+        getting a chance in the area. When I'm not studying I like reading e
+        going to the beach.
+      </p>
+    </div>
+  );
+}
+function SkillsList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="Javascript" emoji="💪" color="blue" />
+      <Skill skill="React" emoji="👶" color="yellow" />
+      <Skill skill="HTML & CSS" emoji="💪" color="red" />
+      <Skill skill="Typescript" emoji="👶" color="green" />
+      <Skill skill="GitHub" emoji="💪" color="purple" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
